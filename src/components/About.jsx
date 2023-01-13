b18e89b96e13bbb0fcb64b27a34ad7ca4abc0fc1
Antonio Daivan <contato.daivan@gmail.com>
2023-01-13 10:41:46 -0300

    correção ortográfica

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export function About() {
       <p>
         Oi, eu sou o Antonio Daivan! Tenho 24 anos e estou iniciando minha carreira
         como desenvolvedor web. Sou Técnico em Rede de Computadores, trabaho em um
-        laboratório de análises clínicas, e, a cerca de um ano escolhi me dedicar a
+        laboratório de análises clínicas, e, há cerca de um ano escolhi me dedicar a
         estudar esse mundo que é o desenvolvimeto de sites, softwares e afins. Tenho
         estudado algumas tecnologias, dentre elas estão as citadas acima e continuo
         buscando me aperfeiçoar. Fico feliz qu tenha chegado até aqui. Abaixo estão
